abc8bf853651a2f2284dc8c1e5b9b93519e73058
Crowdbotics <team@crowdbotics.com>
2021-08-22 11:26:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "akuyweb_30019Identifier",
+  url: "https://akuyweb_30019Identifier.botics.co",
+  api: "https://akuyweb_30019Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
